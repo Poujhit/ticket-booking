62155ed0f8e6f8a0f451746a88e6b4e4167c80ea
Poujhit <poujhit.dev@gmail.com>
2023-02-07 18:52:25 +0530

    feat: done with all the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import dbSchema from './db/dbSchema.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ticket-booking';
+  input = '';
+  coach;
+
+  constructor() {
+    this.coach = structuredClone(dbSchema);
+  }
+
+  onSubmit = () => {
+    console.log(this.coach);
+    console.log(this.input);
+  };
+
+  resetApp() {
+    window.location.reload();
+  }
+
+  // To generate an array to create the seats for each row. ngFor requires an array to iterate
+  getArray(len: number) {
+    return Array(len)
+      .fill(0)
+      .map((_, i) => ++i);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +17,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
